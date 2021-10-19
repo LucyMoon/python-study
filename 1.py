551a0279f8ce4f3fc19ec3345a20181d1439a3a0
LucyMoon <81845462+LucyMoon@users.noreply.github.com>
2021-10-19 10:44:59 +0900

    Day 1
    2021-10-19
    1일차 파이썬 공부(늦게 커밋)

---
+++ 1.py
@@ -0,0 +1,58 @@
+# from tkinter import *
+# w=Tk()
+# w.title("Kim Tae Hwan")
+# w.geometry('720x480')
+# w.mainloop()
+
+# --------------------------------------------------------------------------------------------------
+
+# print(5 < 10)
+# print(5 > 10)
+# print(5 < 10 or 3 > 10)
+# print(5 > 10 and 3 > 10)
+# print(not True)
+# print(not True and True)
+# print(not True or True)
+
+# --------------------------------------------------------------------------------------------------
+
+# name: str = input('이름을 입력하세요 : ')
+# print("my name is", name)
+# print("my name is " + name)
+
+# --------------------------------------------------------------------------------------------------
+
+# a = input('a = ')
+# b = input('b = ')
+# a = int(a)
+# b = int(b)
+#
+# a = int(input("a = "))
+# b = int(input("b = "))
+#
+# print('a + b =', a+b)
+# print('a - b =', a-b)
+# print('a * b =', a*b)
+# print('a / b =', a/b)
+
+# --------------------------------------------------------------------------------------------------
+
+# def Hello():
+#     print("Hello")
+#     print("KIM")
+#
+#
+# Hello()
+
+# --------------------------------------------------------------------------------------------------
+
+# def Hello(name):
+#     print('Hi', name)
+#
+#
+# def Hello1(name):
+#     print(f'Hi {name}')
+#
+#
+# Hello('Kim')
+# Hello1('Kim')

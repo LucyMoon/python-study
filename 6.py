b7b91ce63273be79f30b25c8cb3eedcced34797a
LucyMoon <81845462+LucyMoon@users.noreply.github.com>
2021-11-09 10:29:44 +0900

    Day 6
    2021-11-09
    6일차 파이썬 공부

---
+++ 6.py
@@ -0,0 +1,50 @@
+# def dfs(v):
+#     visited[v] = 1
+#     print(v, end=' ')
+#     for i in graph[v]:
+#         if not visited[i]:
+#             dfs(i)
+#
+# graph = [
+#     [],
+#     [2,3,8],
+#     [1,7],
+#     [1,4,5],
+#     [3,5],
+#     [3,4],
+#     [7],
+#     [2,6,8],
+#     [1,7]
+# ]
+# visited = [0] * 9
+# dfs(1)
+
+
+
+n, m = map(int, input().split())
+graph = []
+for i in range(n):
+    graph.append(list(map(int, input())))
+
+
+def puddle(x, y):
+    if x <= -1 or x >= n or y <= -1 or y >= m:
+        return False
+    if graph[x][y] == 0:
+        graph[x][y] = 1
+        puddle(x - 1, y)
+        puddle(x, y - 1)
+        puddle(x + 1, y)
+        puddle(x, y + 1)
+        return True
+    else:
+        return False
+
+
+cnt = 0
+for i in range(n):
+    for j in range(m):
+        if puddle(i, j) == True:
+            cnt += 1
+
+print(cnt)
